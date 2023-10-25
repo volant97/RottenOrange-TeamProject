15efd0ab6f81600eefb9c964351a0af6f31ff236
dowmddl <mm501y@naver.com>
2023-10-25 16:33:20 +0900

    댓글 ui

--- js/review.js
+++ js/review.js
@@ -78,3 +78,14 @@ export function showReview() {
 }
 
 showReview();
+
+const backToTop = () => {
+  document.getElementById("go-top").addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  });
+};
+backToTop();
