082fc89fa455a72138b054980e1b3f4c1037675b
dowmddl <mm501y@naver.com>
2023-10-27 10:52:12 +0900

    수정

--- js/review.js
+++ js/review.js
@@ -165,14 +165,22 @@ showReview();
 
 // 스크롤 업 기능
 const backToTop = () => {
-  document.getElementById("go-top").addEventListener("click", () => {
-    console.log("click");
+  
+  window.addEventListener('scroll', () => {
+    if (document.querySelector('html').scrollTop > 100) {
+      document.getElementById('go-top').style.display = "block";
+    } else {
+      document.getElementById('go-top').style.display = "none";
+    }
+  });
+  
+  document.getElementById('go-top').addEventListener('click', () => {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: "smooth",
+      behavior: 'smooth'
     });
-  });
+  })
 };
 backToTop();
 
