e764f9c8242f57a772671b8a13e0b5eae550c761
eunbime <ebk1014@gmail.com>
2023-10-28 11:36:58 +0900

    상세페이지 데이터 가져오기

--- js/detailList.js
+++ js/detailList.js
@@ -1,38 +1,33 @@
 export function detailList(detailsListKR, creditsKR) {
-    const detailsList = detailsListKR;
-    const creditsList = creditsKR;
+  const detailsList = detailsListKR;
+  const creditsList = creditsKR;
 
-    const title = detailsList.title;
-    const voteAverage = detailsList.vote_average;
-    const releaseDate = detailsList.release_date;
-    const runTime = detailsList.runtime;
-    const genres = detailsList.genres[0].name;
+  const title = detailsList.title;
+  const voteAverage = detailsList.vote_average;
+  const releaseDate = detailsList.release_date;
+  const runTime = detailsList.runtime;
+  const genres = detailsList.genres[0].name;
 
-    const director = creditsList.crew.find(item => {
-        const job = item.job;
-        if (job === "Director") {
-            return true;
-        }
-    }).name;
-    let actor = creditsList.cast.filter(item => {
-        const department = item.known_for_department;
-        if (department === "Acting") {
-            return true;
-        }
-    }).slice(0, 4)
-    // ----작성중-----
+  const director = creditsList.crew.find((item) => {
+    const job = item.job;
+    if (job === "Director") {
+      return true;
+    }
+  }).name;
 
-    
+  // department === "Acting" 인 경우 배열로 반환
+  let actors = creditsList.cast
+    .filter((item) => {
+      const department = item.known_for_department;
+      if (department === "Acting") {
+        return true;
+      }
+    })
+    .slice(0, 4)
+    .map((actor) => {
+      return actor.name;
+    })
+    .join(", ");
 
-    
-
-    // const actor4 = (actor) => {
-    //     for(let i=0; i<actor.length; i++){
-    //     actor[i]
-    // }
- 
-    const overview = detailsList.overview;
-
-    console.log(actor[0].id);
-    // console.log(actor[0].name + actor[2].name);
+  const overview = detailsList.overview;
 }
